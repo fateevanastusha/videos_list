ca79bc09b668d735c07c0708c7ba8981b26fbac9
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-02-05 16:04:28 +0300

    ain

--- dist/index.js
+++ dist/index.js
@@ -75,103 +75,122 @@ app.post('/videos', (req, res) => {
         publicationDate: (new Date(new Date().setDate(new Date().getDate() + 1)).toISOString()),
         availableResolutions: req.body.availableResolutions
     };
+    let errors_array = [];
     //start checking
-    //first check
-    if (typeof newVideo.title === "string" && typeof newVideo.author === "string" && Array.isArray(newVideo.availableResolutions) && newVideo.availableResolutions.length > 0) {
-        //check for length
-        if (newVideo.title.length > 40 || newVideo.author.length > 20) {
-            res.send(400);
-        }
-        //canBeDownloaded check
-        if (typeof newVideo.canBeDownloaded !== "boolean") {
-            if (!newVideo.canBeDownloaded) {
-                newVideo.canBeDownloaded = false;
-            }
-            else {
-                res.send(400);
-            }
-            //minAgeRestiction check
-            if (!+newVideo.minAgeRestriction) {
-                if (!newVideo.minAgeRestriction) {
-                    newVideo.minAgeRestriction = null;
-                }
-                else {
-                    res.send(400);
-                }
-            }
-            else {
-                if (+newVideo.minAgeRestriction < 1 || +newVideo.minAgeRestriction > 18) {
-                    res.send(400);
-                }
-            }
-        }
-        //availableResolutions check
-        const length = newVideo.availableResolutions.length;
-        newVideo.availableResolutions = newVideo.availableResolutions.filter(value => {
+    //title
+    if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.title) !== "string" || newVideo.title.length > 40) {
+        errors_array.push({ message: "error", field: "title" });
+    }
+    //author
+    if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.author) !== "string" || newVideo.author.length > 20) {
+        errors_array.push({ message: "error", field: "author" });
+    }
+    //availableResolutions
+    if (Array.isArray(newVideo === null || newVideo === void 0 ? void 0 : newVideo.availableResolutions)) {
+        const length = newVideo === null || newVideo === void 0 ? void 0 : newVideo.availableResolutions.length;
+        let checking = newVideo === null || newVideo === void 0 ? void 0 : newVideo.availableResolutions.filter(value => {
             return availableResolutions.includes(value);
         });
-        if (newVideo.availableResolutions.length < length) {
-            res.send(400);
+        if (checking.length < length) {
+            errors_array.push({ message: "error", field: "availableResolutions" });
         }
-        videosDataBase.push(newVideo);
-        res.status(201).send(newVideo);
     }
     else {
-        res.send(400);
+        errors_array.push({ message: "error", field: "availableResolutions" });
+    }
+    //canBeDownloaded
+    if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.canBeDownloaded) !== "boolean") {
+        if ((newVideo === null || newVideo === void 0 ? void 0 : newVideo.canBeDownloaded) === undefined) {
+            newVideo.canBeDownloaded = false;
+        }
+        else {
+            errors_array.push({ message: "error", field: "canBeDownloaded" });
+        }
+    }
+    //minAgeRestriction
+    if ((newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) !== null && typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) !== "number") {
+        if ((newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) === undefined) {
+            newVideo.minAgeRestriction = null;
+        }
+        else {
+            errors_array.push({ message: "error", field: "minAgeRestriction" });
+        }
+    }
+    else if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) === "number") {
+        if (+(newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) < 1 || +(newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) > 18) {
+            errors_array.push({ message: "error", field: "minAgeRestriction" });
+        }
+    }
+    //endpoint
+    if (errors_array.length > 0) {
+        res.status(400).send(errors_array);
+    }
+    else {
+        videosDataBase.push(newVideo);
+        res.status(201).send(newVideo);
     }
 });
 //update video
 app.put('/videos/:id', (req, res) => {
     let newVideo = videosDataBase.find(p => p.id === +req.params.id);
     let index = videosDataBase.findIndex(p => p.id === +req.params.id);
+    let errors_array = [];
     //add by value
     if (newVideo) {
         newVideo = Object.assign(Object.assign({}, newVideo), req.body);
         //check for correct data
-        if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.title) === "string" && typeof newVideo.author === "string" && Array.isArray(newVideo.availableResolutions) && newVideo.availableResolutions.length > 0) {
-            //check for length
-            if (newVideo.title.length > 40 || newVideo.author.length > 20) {
-                res.send(400);
-            }
-            //canBeDownloaded check
-            if (typeof newVideo.canBeDownloaded !== "boolean") {
-                if (!newVideo.canBeDownloaded) {
-                }
-                else {
-                    res.send(400);
-                }
-            }
-            //minAgeRestiction check
-            if (newVideo.minAgeRestriction) {
-                if (newVideo.minAgeRestriction < 1 || newVideo.minAgeRestriction > 18) {
-                    res.send(400);
-                }
-            }
-            //availableResolutions check
-            const length = newVideo.availableResolutions.length;
-            newVideo.availableResolutions = newVideo.availableResolutions.filter(value => {
+        //title
+        if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.title) !== "string" || newVideo.title.length > 40) {
+            errors_array.push({ message: "error", field: "title" });
+        }
+        //author
+        if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.author) !== "string" || newVideo.author.length > 20) {
+            errors_array.push({ message: "error", field: "author" });
+        }
+        //availableResolutions
+        if (Array.isArray(newVideo === null || newVideo === void 0 ? void 0 : newVideo.availableResolutions)) {
+            const length = newVideo === null || newVideo === void 0 ? void 0 : newVideo.availableResolutions.length;
+            let checking = newVideo === null || newVideo === void 0 ? void 0 : newVideo.availableResolutions.filter(value => {
                 return availableResolutions.includes(value);
             });
-            if (newVideo.availableResolutions.length < length) {
-                res.send(400);
+            if (checking.length < length) {
+                errors_array.push({ message: "error", field: "availableResolutions" });
             }
-            //publicationDate check
-            if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.publicationDate) !== "string") {
-                res.send(400);
+        }
+        else {
+            errors_array.push({ message: "error", field: "availableResolutions" });
+        }
+        //canBeDownloaded
+        if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.canBeDownloaded) !== "boolean") {
+            errors_array.push({ message: "error", field: "canBeDownloaded" });
+        }
+        //minAgeRestriction
+        if ((newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) !== null && typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) !== "number") {
+            errors_array.push({ message: "error", field: "minAgeRestriction" });
+        }
+        else if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) === "number") {
+            if (+(newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) < 1 || +(newVideo === null || newVideo === void 0 ? void 0 : newVideo.minAgeRestriction) > 18) {
+                errors_array.push({ message: "error", field: "minAgeRestriction" });
             }
-            //publicationDate check for format
+        }
+        //publicationDate
+        if (typeof (newVideo === null || newVideo === void 0 ? void 0 : newVideo.publicationDate) === "string") {
             let r = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
             if (!r.test(newVideo.publicationDate)) {
-                res.send(404);
+                errors_array.push({ message: "error", field: "publicationDate" });
             }
-            //assigning 
+        }
+        else {
+            errors_array.push({ message: "error", field: "publicationDate" });
+        }
+        //assigment to variable
+        if (errors_array.length > 0) {
+            res.status(400).send(errors_array);
+        }
+        else {
             videosDataBase[index] = newVideo;
             res.send(204);
         }
-        else {
-            res.send(400);
-        }
-        ;
     }
     else {
         res.send(404);
